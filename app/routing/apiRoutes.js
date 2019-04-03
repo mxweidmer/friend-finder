85d86860dea5ee8d495623808834e6cfb987186a
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-02 23:26:32 -0700

    completed api get route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,15 @@
 var express = require("express");
+var path = require("path");
+
+var users = require(path.join(__dirname, "../data/friends.js"));
 
 var apiRouter = express.Router();
 
-apiRouter.get("/api/friends", function(req, res) {
+apiRouter.get("/api/friends", function (req, res) {
+    res.json(users);
+})
+
+apiRouter.post("/api/friends", function (req, res) {
     console.log("SOMETHING TO SEE THIS IS WORKING")
 })
 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/app/public/')));
 app.use(htmlRouter);
 app.use(apiRouter);
 
