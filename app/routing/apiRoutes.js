591c7f10325caa7605342efd2c940265f7a1dab7
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-04-02 23:53:01 -0700

    working on post request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,21 @@ apiRouter.get("/api/friends", function (req, res) {
 })
 
 apiRouter.post("/api/friends", function (req, res) {
-    console.log("SOMETHING TO SEE THIS IS WORKING")
+    console.log(req.body);
+    var results = [];
+    for (var i = 0; i < users.length; i++) {
+        var score = 0;
+        for (var j = 0; j < users[i].scores.length; j++) {
+
+            score += Math.abs(parseInt(req.body.scores[j]) - parseInt(users[i].scores[j]));
+            console.log(score);
+
+        }
+        results.push(parseInt(score));
+    }
+    console.log(results);
+    results.sort(function (a, b) { return a - b });
+    console.log(results);
 })
 
 module.exports = apiRouter;
